abf8044890f0ea1244d6027b378ea16a56ce44f4
github VinousTy <github ys.taka44@gmail.com>
2024-07-15 15:47:51 +0900

    refactor:terraform fmt

--- envs/dev/security_group.tf
+++ envs/dev/security_group.tf
@@ -7,23 +7,23 @@ resource "aws_security_group" "app" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port  = 80
-    to_port    = 80
-    protocol   = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port  = 443
-    to_port    = 443
-    protocol   = "tcp"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port  = 0
-    to_port    = 0
-    protocol   = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -41,16 +41,16 @@ resource "aws_security_group" "vpc" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port  = 0
-    to_port    = 0
-    protocol   = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port  = 0
-    to_port    = 0
-    protocol   = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
